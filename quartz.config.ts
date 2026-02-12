ee3b81cdd1d24607e7520d8bc1dc8584403d4742
dwids <sidwell.david@gmail.com>
2026-02-12 18:43:25 +1100

    Obs Git plugin manual backup 2026-02-12 18:43:25  changed: M quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dwids.github.io/sic",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
