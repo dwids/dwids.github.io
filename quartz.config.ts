2f89e228228bab29161ae8be10b94ea33ba888ed
dwids <sidwell.david@gmail.com>
2026-02-18 15:20:38 +1100

    Move site to root domain, no longer '../sic'

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dwids.github.io/sic",
+    baseUrl: "dwids.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
