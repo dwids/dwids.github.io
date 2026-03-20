05886eab037df6cd9156ccc43b3068b11d5846c1
dwids <sidwell.david@gmail.com>
2026-03-21 09:31:49 +1100

    Obs Git plugin manual backup 2026-03-21 09:31:49  changed: M quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,6 +16,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.MobileOnly(Component.Search()),
     Component.ConditionalRender({
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
@@ -50,7 +51,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.MobileOnly(Component.Search()),Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
  //   Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
