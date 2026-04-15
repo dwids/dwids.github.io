bdf1ac714739fbf6a6228b88ca0276a6706530b7
dwids <sidwell.david@gmail.com>
2026-04-16 07:54:52 +1000

    Site update

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "DS Quartz 4",
+    pageTitle: "DS Quartz 5",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
